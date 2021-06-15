5cb0147fb073d965c6ee8ddf0ce1fd713b79015c
Ben Arno <bdarno92@gmail.com>
2021-06-14 18:29:49 -0700

    form done maybe

--- src/LocationForm.js
+++ src/LocationForm.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from'axios';
+
 
 class LocationForm extends React.Component {
+  constructor(props) {
+    super(props) 
+      this.state = {
+        city: '',
+        displayName: '',
+      };
+    }
+
+  hangleChange = e => {
+    this.setState({city: e.target.value})
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await axios.get(`https://us1.locationiq.com/v1/search.php?${process.env.REACT_APP_CITY_KEY}&q=${this.state.city}&format=json`)
+    const cityInfo = response.data[0];
+    let displayName = cityInfo.display_name;
+    this.setState({displayName});
+  }
+  
+
   render() {
     return(
       <>
+        <p>Type in a city name</p>
         <Form>
-          <Form.Group>
-            <Form.Control as="textarea" placeholder="Location">
-            </Form.Control>
-              <Button variant="primary" type="submit">Explore!</Button>
-          </Form.Group>
+          <input name="city" onChange={this.handleChange} />
+          <Button variant="primary" type="submit">Explore!</Button>
         </Form>
       </>
     )
